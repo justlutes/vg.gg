e3c2074b56c5478f289c8327be4cbe61632a6a65
Kyle Lutes <klutes90@gmail.com>
2017-11-10 16:48:50 -0500

    separating match data

--- molecules/Matches.js
+++ molecules/Matches.js
@@ -25,7 +25,7 @@ export default class Matches extends React.Component {
       hero: rosterData.data.attributes.actor.replace(/[^\w\s]/gi, ''),
       kda: `${rosterData.data.attributes.stats.kills}/${rosterData.data
         .attributes.stats.deaths}/${rosterData.data.attributes.stats.assists}`,
-      gold: rosterData.data.attributes.stats.gold.toFixed(2),
+      gold: rosterData.data.attributes.stats.gold.toFixed(1),
       items: rosterData.data.attributes.stats.items,
       cs: rosterData.data.attributes.stats.minionKills,
       side: playerData.stats.side,
@@ -43,33 +43,78 @@ export default class Matches extends React.Component {
       teams[i] = team;
     });
 
-    console.log(player, teams);
     this.setState({ player, teams });
   }
   render() {
     const { duration, patch, mode } = this.props;
+    const { player, teams } = this.state;
+
     return (
-      <Row>
-        <div>Hero</div>
-        <div>
-          Victory/Time
-          {duration}
+      <Row classname="columns">
+        <Hero classname="column">{player.hero}</Hero>
+        <Game className="column">
+          <Result result={player.result}>{player.result}</Result>
           {mode}
-        </div>
-        <div>KDA</div>
-        <div>Stats {patch}</div>
-        <div>Build</div>
+          {duration}
+        </Game>
+        <Build classname="column">
+          {player.items.map(item => <span key={item}>{item}</span>)}
+        </Build>
+        <KDA className="column">
+          <span>{player.kda}</span>
+          <span>KDA</span>
+        </KDA>
+        <Stats className="column">
+          <Stat>
+            <span>{patch}</span>
+            <span>Patch</span>
+          </Stat>
+          <Stat>
+            <span>{player.cs}</span>
+            <span>CS</span>
+          </Stat>
+          <Stat>
+            <span>{player.gold}</span>
+            <span>Gold</span>
+          </Stat>
+        </Stats>
         <div>Roster</div>
       </Row>
     );
   }
 }
 
-const Row = styled.div`
+const Build = styled.div`
   display: flex;
+  flex-direction: row;
+  align-items: center;
+  flex: nowrap;
+`;
+
+const Hero = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const Game = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const KDA = Game.extend``;
+
+const Result = styled.span`
+  color: ${props => (props.result === 'Victory' ? 'green' : 'red')};
+`;
+
+const Row = styled.div`
   align-items: center;
   justify-content: space-around;
   background: #383838;
   padding: 15px;
   border-bottom: 1px solid RGBA(233, 233, 233, 0.15);
 `;
+
+const Stat = Game.extend``;
+
+const Stats = Hero.extend`justify-content: space-around;`;
